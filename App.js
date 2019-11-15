370b1e272174c8b564a3a173be235fc285120b7b
karczewskipiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2019-11-15 13:56:10 +0100

    Implemented basics of navigation for signing up and in to the application.

--- App.js
+++ App.js
@@ -2,15 +2,40 @@ import React from 'react';
 import { ApplicationProvider, IconRegistry } from 'react-native-ui-kitten';
 import { mapping, light as theme } from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import SignUpScreen from './src/screens/sign-up';
+import SignInScreen from './src/screens/sign-in';
 import Navigation from './src/layout/bottom-navigation';
 
-const App = () => (
-  <React.Fragment>
-    <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider mapping={mapping} theme={theme}>
-      <Navigation />
-    </ApplicationProvider>
-  </React.Fragment>
-);
+const App = () => {
+  const AppContainer = getAppContainer();
+
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider mapping={mapping} theme={theme}>
+        <AppContainer />
+      </ApplicationProvider>
+    </>
+  );
+};
+
+const getAppContainer = passedProps => {
+  const Navigator = createStackNavigator(
+    {
+      SignUp: { screen: props => <SignUpScreen {...props} {...passedProps}/> },
+      SignIn: { screen: props => <SignInScreen {...props} {...passedProps}/> },
+      App: Navigation,
+    },
+    {
+      initialRouteName: 'SignUp',
+      headerMode: 'none',
+    },
+  );
+
+  return createAppContainer(Navigator);
+};
 
 export default App;
