7180e200ebd8dfcde75f4c7cbdcb76f3cd3dbcd0
Łukasz Starosta <lstarosta98@gmail.com>
2019-12-03 19:53:48 +0100

    Explore view

---
+++ src/components/category.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Layout, Text, Avatar } from 'react-native-ui-kitten';
+
+function Category(props) {
+  const { categoryName } = props;
+  return (
+    <Layout style={styles.container}>
+      <Avatar
+        style={styles.image}
+        source={{
+          uri: 'https://i.imgur.com/2y3Sm4x.jpg',
+        }}
+      />
+      <Text style={styles.categoryName}>{categoryName}</Text>
+    </Layout>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 8,
+    marginHorizontal: 4,
+  },
+  image: {
+    width: 108,
+    height: 108,
+    borderRadius: 10,
+    marginRight: 8,
+  },
+  categoryName: {
+    marginTop: 2,
+    fontSize: 14,
+    maxWidth: 120,
+    fontWeight: 'bold',
+    color: '#5a5a5a',
+  },
+});
+
+export default Category;
--- src/screens/explore.js
+++ src/screens/explore.js
@@ -1,19 +1,56 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Text, Layout } from 'react-native-ui-kitten';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import ScreenContainer from '../layout/screen-container';
 import SearchBar from '../components/search-bar';
+import Club from '../components/club';
+import { SectionTitle } from '../components/texts-containers';
+import Category from '../components/category';
 
 function ExploreScreen() {
   return (
     <ScreenContainer title="Przeglądaj">
-      <Layout>
+      <View>
         <SearchBar placeholder="Szukaj..." />
-      </Layout>
+      </View>
+      <View style={styles.clubsSection}>
+        <SectionTitle>Proponowane</SectionTitle>
+        <Club />
+        <Club />
+        <Club />
+      </View>
+      <View style={styles.categoriesSection}>
+        <SectionTitle>Wydziały</SectionTitle>
+        <ScrollView horizontal style={styles.categoriesContainer}>
+          <Category categoryName={'Wydział'} />
+          <Category categoryName={'Długi Wydział'} />
+          <Category categoryName={'Bardzo długi Wydział'} />
+          <Category categoryName={'Bardzo długi Wydział'} />
+        </ScrollView>
+      </View>
+      <View style={styles.categoriesSection}>
+        <SectionTitle>Kategorie</SectionTitle>
+        <ScrollView horizontal style={styles.categoriesContainer}>
+          <Category categoryName={'Informatyka'} />
+          <Category categoryName={'Nadrutach dzierganie'} />
+          <Category categoryName={'Drogi panie marianie'} />
+          <Category categoryName={'Jak przeskrolóje'} />
+          <Category categoryName={'Toco siestanie'} />
+        </ScrollView>
+      </View>
     </ScreenContainer>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  clubsSection: {
+    marginVertical: 16,
+  },
+  categoriesSection: {},
+  categoriesContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+  },
+});
 
 export default ExploreScreen;
