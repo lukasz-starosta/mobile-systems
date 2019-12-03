ed14d4126839a3dfaac71a6e44baceb639b60173
Łukasz Starosta <lstarosta98@gmail.com>
2019-12-03 19:16:50 +0100

    Added search input

---
+++ src/components/search-bar.js
@@ -0,0 +1,54 @@
+import React, { useRef } from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+import { Text, Layout, Icon } from 'react-native-ui-kitten';
+import colors from '../constants/colors';
+
+function SearchBar(props) {
+  const { placeholder } = props;
+
+  return (
+    <Layout style={styles.searchBar}>
+      <Icon
+        style={styles.icon}
+        width="24"
+        height="24"
+        name="search-outline"
+        fill="white"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        placeholderTextColor="white"
+      />
+    </Layout>
+  );
+}
+
+const styles = StyleSheet.create({
+  searchBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: colors.politechnika,
+    height: 40,
+    borderRadius: 30,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    marginLeft: 12,
+  },
+  input: {
+    position: 'absolute',
+    left: 0,
+    color: '#ffffff',
+    height: '100%',
+    width: '100%',
+    padding: 0,
+    paddingLeft: 40,
+    borderWidth: 0,
+    backgroundColor: 'transparent',
+  },
+});
+
+export default SearchBar;
--- src/screens/explore.js
+++ src/screens/explore.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { Text, Layout } from 'react-native-ui-kitten';
 import ScreenContainer from '../layout/screen-container';
+import SearchBar from '../components/search-bar';
 
 function ExploreScreen() {
   return (
     <ScreenContainer title="Przeglądaj">
-      <Text>wujeczku pzejrzyj se</Text>
+      <Layout>
+        <SearchBar placeholder="Szukaj..." />
+      </Layout>
     </ScreenContainer>
   );
 }
+
+const styles = StyleSheet.create({});
 
 export default ExploreScreen;
