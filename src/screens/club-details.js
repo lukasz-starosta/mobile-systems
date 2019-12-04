d96603778fdc4a019983574c7b4cadc30d49ff3e
karczewskipiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2019-12-04 23:18:33 +0100

    <Club

---
+++ src/screens/club-details.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import ScreenContainer from '../layout/screen-container'
+
+const ClubDetailsScreen = () => {
+  return (
+    <ScreenContainer>
+      
+    </ScreenContainer>
+  );
+};
+
+export default ClubDetailsScreen;
