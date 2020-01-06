786c709cb543aeb9a4b1ba423b6c0f201ee54207
karczewskipiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2020-01-06 15:27:25 +0100

    kategorie maja logo

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,33 +1,55 @@
 export const categories = [
-  { name: 'Informatyka' },
+  {
+    name: 'Informatyka',
+    icon:
+      'https://apollo-ireland.akamaized.net/v1/files/avxapx7lnzcn3-PL/image;s=644x461',
+  },
   {
     name: 'Mechanika',
+    icon:
+      'http://www.klinikamechanika.pl/images/klinika-dobrego-mechanika_phone1.jpg?crc=4039233064',
   },
   {
     name: 'Sport',
+    icon:
+      'https://i.wpimg.pl/O/644x430/d.wpimg.pl/1121730977-215315527/jogging.jpg',
   },
   {
     name: 'Elektronika',
+    icon:
+      'https://mlodytechnik.pl/i/images/1/2/4/Y3c9MTE3MCZjaD01MTM=_src_2124-elektronika.jpg',
   },
   {
     name: 'Wzornictwo',
+    icon:
+      'https://www.otouczelnie.pl/assets/uploads/strona/c938e-wzornictwo-studia.jpg',
   },
   {
     name: 'Rysunek',
+    icon: 'http://kurs-rysunku.pl/images/kwadraty/wzornictwo3.png',
   },
   {
     name: 'Pojazdy',
+    icon: 'https://bi.im-g.pl/im/0b/a2/17/z24782859Q,Triggo.jpg',
   },
   {
     name: 'Muzyka',
+    icon:
+      'https://g0.gazetaprawna.pl/p/_wspolne/pliki/2839000/2839886-657-323.jpg',
   },
   {
     name: 'Fotografia',
+    icon:
+      'https://nowyswiatnieruchomosci.pl/wp-content/uploads/2018/04/fotografia-nieruchomosci-1.jpg',
   },
   {
     name: 'Organizacje',
+    icon:
+      'https://www.glassdoor.com/blog/app/uploads/sites/2/iStock-520085758-750x370.jpg',
   },
   {
     name: 'Podróże',
+    icon:
+      'https://i.wpimg.pl/O/644x430/d.wpimg.pl/241893749--477772744/filipiny.jpg',
   },
 ];
