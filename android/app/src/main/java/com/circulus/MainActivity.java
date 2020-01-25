2b69122a6caed1dfe9078c148dd269abdf2947d3
Łukasz Starosta <lstarosta98@gmail.com>
2020-01-25 13:51:00 +0100

    App name and round icons

--- android/app/src/main/java/com/circulus/MainActivity.java
+++ android/app/src/main/java/com/circulus/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mobilesystems;
+package com.circulus;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "MobileSystems";
+        return "Circulus";
     }
 }
--- android/app/src/main/java/com/circulus/MainApplication.java
+++ android/app/src/main/java/com/circulus/MainApplication.java
@@ -1,4 +1,4 @@
-package com.mobilesystems;
+package com.circulus;
 
 import android.app.Application;
 import android.util.Log;
